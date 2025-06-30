b4701e57d057b8014e0a819fffeb69cc234c6cb8
Jisang Yun <journeyofbabo@gmail.com>
2025-06-30 21:53:15 +0900

    creating model

--- torch_basic/quick_start.py
+++ torch_basic/quick_start.py
@@ -1,5 +1,6 @@
 import torch
-from torch import nn 
+print(torch.__version__)
+from torch import backends, nn 
 from torch.utils.data import DataLoader
 from torchvision import datasets 
 from torchvision.transforms import ToTensor
@@ -30,3 +31,36 @@ for X, y in test_dataloader:
     print(f"Shape of X[N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
+
+# Creating Models 
+# device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+        )
+print(f"Using {device} device")
+
+# Define model 
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+                nn.Linear(28*28, 512),
+                nn.ReLU(),
+                nn.Linear(512, 512),
+                nn.ReLU(),
+                nn.Linear(512, 10),
+                )
+
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits 
+
+model = NeuralNetwork().to(device)
+print(model)
+
