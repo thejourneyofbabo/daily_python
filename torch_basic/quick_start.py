d95becbeb7114ddef4dbf015b0b834e3f03b8970
Jisang Yun <journeyofbabo@gmail.com>
2025-07-01 09:23:06 +0900

    update gitignore

--- torch_basic/quick_start.py
+++ torch_basic/quick_start.py
@@ -113,3 +113,33 @@ for t in range(epochs):
 
 print("Done!")
 
+
+## Saving Models
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+
+## Loading Models
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth", weights_only=True))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)    
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
